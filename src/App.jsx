77359fd6a2f35c9a7c5bcc64e447c73e604acecc
Degover <chaors2000@gmail.com>
2021-06-01 20:28:04 -0300

    Componente do card completo, aula 23

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './index.css'
+import './App.css'
 import React from 'react'
 
 import Card from './components/layout/Card'
@@ -8,22 +9,23 @@ import ComParametro from './components/basicos/ComParametro'
 import Aleatorio from './components/basicos/Aleatorio'
 
 export default () =>
-    <div>
+    <div className="app">
         <h1>Fundamentos react</h1>
 
-        <Card titulo="#4 Desafio Aleatório">
-            <Aleatorio max={10} min={2} decimalNums={2}/>
-        </Card>
+        <div className="cards-container">
+            <Card titulo="#4 Desafio Aleatório" color="#FA6900">
+                <Aleatorio max={10} min={2} decimalNums={2} />
+            </Card>
 
-        <Card titulo="#2 Com parametro">
-            <ComParametro
-                titulo="Segundo componente"
-                aluno="Pedro"
-                nota={9.3} />
-        </Card>
+            <Card titulo="#2 Com parametro" color="#E94C6F">
+                <ComParametro
+                    titulo="Segundo componente"
+                    aluno="Pedro"
+                    nota={9.3} />
+            </Card>
 
-        <Card titulo="#1 Primeiro">
-            <Primeiro></Primeiro>
-        </Card>
-
+            <Card titulo="#1 Primeiro componente" color="#588C73">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
     </div>
--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,8 +2,14 @@ import './Card.css'
 import React from 'react';
 
 export default props => {
+    const color = props.color || '#F00'
+    const cardStyle = {
+        backgroundColor: color,
+        borderColor: color
+    }
+
     return (
-        <div className="card">
+        <div className="card" style={cardStyle}>
             <div className="title">{props.titulo}</div>
             <div className="content">
                 { props.children }
